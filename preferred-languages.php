0095b84cd358e4392d04b24d0da1bf2ea77531a4
Pascal Birchler <hello@pascalbirchler.ch>
2018-07-27 19:07:50 +0200

    Version 1.4.0

--- preferred-languages.php
+++ preferred-languages.php
@@ -3,7 +3,7 @@
  * Plugin Name: Preferred Languages
  * Plugin URI:  https://github.com/swissspidy/preferred-languages/
  * Description: Choose languages for displaying WordPress in, in order of preference.
- * Version:     1.4.0-beta
+ * Version:     1.4.0
  * Author:      Pascal Birchler
  * Author URI:  https://pascalbirchler.com
  * License:     GPL-2.0+
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: internationalization, i18n, localization, l10n, language, locale, translat
 Requires at least: 4.9
 Tested up to: 4.9
 Requires PHP: 5.2
-Stable tag: 1.3.0
+Stable tag: 1.4.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
