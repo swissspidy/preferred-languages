2b6bc96b10a04f64ecf313e3fd7ba5fa3f9c6e96
Pascal Birchler <pascalb@google.com>
2021-03-09 17:30:18 +0100

    Lint fixes

--- tests/e2e/specs/profile.js
+++ tests/e2e/specs/profile.js
@@ -1,8 +1,8 @@
 import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
 describe( 'User Profile', () => {
-	it('should display the preferred languages form', async () => {
+	it( 'should display the preferred languages form', async () => {
 		await visitAdminPage( 'profile.php' );
-		await expect(page).toMatchElement('.user-preferred-languages-wrap');
-	});
-});
+		await expect( page ).toMatchElement( '.user-preferred-languages-wrap' );
+	} );
+} );
--- tests/e2e/specs/settings.js
+++ tests/e2e/specs/settings.js
@@ -1,8 +1,8 @@
 import { visitAdminPage } from '@wordpress/e2e-test-utils';
 
 describe( 'Settings Page', () => {
-	it('should display the preferred languages form', async () => {
+	it( 'should display the preferred languages form', async () => {
 		await visitAdminPage( 'options-general.php' );
-		await expect(page).toMatchElement('.site-preferred-languages-wrap');
-	});
-});
+		await expect( page ).toMatchElement( '.site-preferred-languages-wrap' );
+	} );
+} );
