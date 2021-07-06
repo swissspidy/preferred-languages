88b0b6bbb70f10720522108401b9071831202d45
Pascal Birchler <pascalb@google.com>
2021-07-06 12:13:21 +0200

    Add `@covers` annotations to tests

--- tests/phpunit/tests/plugin.php
+++ tests/phpunit/tests/plugin.php
@@ -1,6 +1,9 @@
 <?php
 
 class Plugin_Test extends WP_UnitTestCase {
+	/**
+	 * @covers ::preferred_languages_register_setting
+	 */
 	public function test_register_setting() {
 		preferred_languages_register_setting();
 		$this->assertArrayHasKey( 'preferred_languages', get_registered_settings() );
@@ -10,6 +13,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @covers ::preferred_languages_register_meta
+	 */
 	public function test_register_meta() {
 		preferred_languages_register_meta();
 		$this->assertTrue( registered_meta_key_exists( 'user', 'preferred_languages' ) );
@@ -19,6 +25,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		);
 	}
 
+	/**
+	 * @covers ::preferred_languages_update_user_option
+	 */
 	public function test_update_user_option() {
 		$user_id = self::factory()->user->create( [
 			'role' => 'administrator',
@@ -30,6 +39,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( 'de_DE,fr_FR', $actual );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_user_list
+	 */
 	public function test_get_user_list() {
 		$user_id = self::factory()->user->create( [
 			'role' => 'administrator',
@@ -46,6 +58,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_user_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_user_list
+	 */
 	public function test_get_user_list_falls_back_to_locale_user_meta() {
 		$user_id = self::factory()->user->create( [
 			'role' => 'administrator',
@@ -61,6 +76,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_user_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_site_list
+	 */
 	public function test_get_site_list() {
 		update_option( 'preferred_languages', 'de_DE,fr_FR' );
 
@@ -72,6 +90,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_site_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_site_list
+	 */
 	public function test_get_list() {
 		$user_id = self::factory()->user->create( [
 			'role' => 'administrator',
@@ -86,6 +107,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_list
+	 */
 	public function test_get_list_admin() {
 		set_current_screen( 'index.php' );
 
@@ -105,6 +129,9 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_get_list
+	 */
 	public function test_get_list_admin_fallback() {
 		set_current_screen( 'index.php' );
 
@@ -121,26 +148,41 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertSame( $expected, preferred_languages_get_list() );
 	}
 
+	/**
+	 * @covers ::preferred_languages_filter_locale
+	 */
 	public function test_filter_locale_returns_locale_unchanged() {
 		$this->assertSame( 'de_CH', preferred_languages_filter_locale( 'de_CH' ) );
 	}
 
+	/**
+	 * @covers ::preferred_languages_filter_locale
+	 */
 	public function test_filter_locale_returns_first_locale() {
 		update_option( 'preferred_languages', 'de_CH,fr_FR' );
 		$this->assertSame( 'de_CH', preferred_languages_filter_locale( 'de_DE' ) );
 	}
 
+	/**
+	 * @covers ::preferred_languages_init_registry
+	 */
 	public function test_init_registry() {
 		preferred_languages_init_registry();
 		$this->assertInstanceOf( Preferred_Languages_Textdomain_Registry::class, $GLOBALS['preferred_languages_textdomain_registry'] );
 	}
 
+	/**
+	 * @covers ::preferred_languages_register_scripts
+	 */
 	public function test_register_scripts()	{
 		preferred_languages_register_scripts();
 		$this->assertTrue( wp_script_is ( 'preferred-languages', 'registered' ) );
 		$this->assertTrue( wp_style_is ( 'preferred-languages', 'registered' ) );
 	}
 
+	/**
+	 * @covers ::preferred_languages_personal_options
+	 */
 	public function test_personal_options() {
 		$user_id = self::factory()->user->create( [
 			'role' => 'administrator',
@@ -154,6 +196,7 @@ class Plugin_Test extends WP_UnitTestCase {
 
 	/**
 	 * @group ms-excluded
+	 * @covers ::preferred_languages_personal_options
 	 */
 	public function test_personal_options_no_languages() {
 		$user_id = self::factory()->user->create( [
@@ -172,12 +215,18 @@ class Plugin_Test extends WP_UnitTestCase {
 		$this->assertNotEmpty( $output );
 	}
 
+	/**
+	 * @covers ::preferred_languages_personal_options
+	 */
 	public function test_personal_options_no_capability() {
 		$output = get_echo( 'preferred_languages_personal_options', [ wp_get_current_user() ] );
 
 		$this->assertNotEmpty( $output );
 	}
 
+	/**
+	 * @covers ::preferred_languages_personal_options
+	 */
 	public function test_personal_options_no_languages_and_no_capability() {
 		add_filter( 'get_available_languages', '__return_empty_array' );
 
