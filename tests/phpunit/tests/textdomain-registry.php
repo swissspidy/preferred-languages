1249f739bf850eacf5ee76f84799fac7a5764bd9
Pascal Birchler <pascalb@google.com>
2021-07-07 19:55:32 +0200

    Add test for theme text domain

--- tests/phpunit/tests/textdomain-registry.php
+++ tests/phpunit/tests/textdomain-registry.php
@@ -14,14 +14,27 @@ class Textdomain_Registry_Test extends WP_UnitTestCase {
 	}
 
 	/**
+	 * @covers ::get
 	 * @covers ::get_path_from_lang_dir
 	 * @covers ::set_cached_mo_files
 	 */
-	public function test_get_path_from_lang_dir() {
+	public function test_get_path_from_lang_dir_plugin() {
 		update_option( 'preferred_languages', 'de_DE,fr_FR' );
 		$registry = new Preferred_Languages_Textdomain_Registry();
 		$this->assertNotFalse( $registry->get( 'internationalized-plugin' ) );
 		$this->assertStringEndsWith( WP_LANG_DIR . '/plugins/', $registry->get( 'internationalized-plugin' ) );
+	}
+
+	/**
+	 * @covers ::get
+	 * @covers ::get_path_from_lang_dir
+	 * @covers ::set_cached_mo_files
+	 */
+	public function test_get_path_from_lang_dir_theme() {
+		update_option( 'preferred_languages', 'de_DE,fr_FR' );
+		$registry = new Preferred_Languages_Textdomain_Registry();
+		$this->assertNotFalse( $registry->get( 'internationalized-theme' ) );
+		$this->assertStringEndsWith( WP_LANG_DIR . '/themes/', $registry->get( 'internationalized-theme' ) );
 	}
 
 	/**
