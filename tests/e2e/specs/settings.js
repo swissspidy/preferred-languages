aea0c5007f1f2f8368f4c470f644bf1558e4e3d3
Pascal Birchler <pascalb@google.com>
2021-07-07 23:50:53 +0200

    Fix test

--- tests/e2e/specs/settings.js
+++ tests/e2e/specs/settings.js
@@ -14,22 +14,13 @@ describe( 'Settings Page', () => {
 
 		// Form buttons disabled by default.
 		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Move Up/i,
-			}
+			'.preferred-languages button.locales-move-up[disabled]'
 		);
 		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Move Down/i,
-			}
+			'.preferred-languages button.locales-move-down[disabled]'
 		);
 		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Remove/i,
-			}
+			'.preferred-languages button.locales-remove[disabled]'
 		);
 
 		const inactiveLocale = await page.$eval(
@@ -40,9 +31,9 @@ describe( 'Settings Page', () => {
 		// Afrikaans is the first item in the dropdown by default.
 		expect( inactiveLocale ).toStrictEqual( 'af' );
 
-		await expect( page ).toClick( '.preferred-languages button', {
-			text: /Add/i,
-		} );
+		await expect( page ).toClick(
+			'.preferred-languages button.locales-add'
+		);
 
 		// After adding Afrikaans to the list, only the "Remove" button should be active.
 		await expect( page ).toMatchElement( '.active-locale', {
@@ -50,70 +41,67 @@ describe( 'Settings Page', () => {
 		} );
 
 		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Move Up/i,
-			}
+			'.preferred-languages button.locales-move-up[disabled]'
 		);
 		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Move Down/i,
-			}
+			'.preferred-languages button.locales-move-down[disabled]'
 		);
 		await expect( page ).toMatchElement(
-			'.preferred-languages button:not([disabled])',
-			{
-				text: /Remove/i,
-			}
+			'.preferred-languages button.locales-remove:not([disabled])'
 		);
 
-		await expect( page ).toClick( '.preferred-languages button', {
-			text: /Add/i,
-		} );
-		await expect( page ).toClick( '.preferred-languages button', {
-			text: /Add/i,
-		} );
+		await expect( page ).toClick(
+			'.preferred-languages button.locales-add'
+		);
+		await expect( page ).toClick(
+			'.preferred-languages button.locales-add'
+		);
+
+		const activeLocales = await page.$eval(
+			'input[name="preferred_languages"]',
+			( el ) => el.value
+		);
+
+		expect( activeLocales ).toStrictEqual( 'af,ar,ary' );
 
 		// After adding two more locales, the last one should be selected.
 
 		await expect( page ).toMatchElement(
-			'.preferred-languages button:not([disabled])',
+			'.active-locale[aria-selected="true"]',
 			{
-				text: /Move Up/i,
-			}
-		);
-		await expect( page ).toMatchElement(
-			'.preferred-languages button[disabled]',
-			{
-				text: /Move Down/i,
-			}
-		);
-		await expect( page ).toMatchElement(
-			'.preferred-languages button:not([disabled])',
-			{
-				text: /Remove/i,
+				text: 'العربية المغربية',
 			}
 		);
 
-		// After moving one up, none of the buttons should be disabled anymore.
+		await expect( page ).toMatchElement(
+			'.preferred-languages button.locales-move-up:not([disabled])'
+		);
+		await expect( page ).toMatchElement(
+			'.preferred-languages button.locales-move-down[disabled]'
+		);
+		await expect( page ).toMatchElement(
+			'.preferred-languages button.locales-remove:not([disabled])'
+		);
+
+		// After moving one position up, none of the buttons should be disabled anymore.
 		await page.keyboard.press( 'ArrowUp' );
 
 		await expect( page ).toMatchElement(
-			'.preferred-languages button:not([disabled])',
-			{
-				text: /Move Down/i,
-			}
+			'.preferred-languages button.locales-move-down:not([disabled])'
 		);
 
 		// After moving one up again, the "Move Up" button should be disabled because we reached the top.
 		await page.keyboard.press( 'ArrowUp' );
 
 		await expect( page ).toMatchElement(
-			'.preferred-languages button:not([disabled])',
+			'.active-locale[aria-selected="true"]',
 			{
-				text: /Move Up/i,
+				text: 'Afrikaans',
 			}
+		);
+
+		await expect( page ).toMatchElement(
+			'.preferred-languages button.locales-move-up[disabled]'
 		);
 	} );
 } );
