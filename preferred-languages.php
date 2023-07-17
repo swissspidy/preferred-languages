f6a2cd7a4ffcd5664facfab49b0be2eb3693543e
Pascal Birchler <pascal.birchler@gmail.com>
2023-07-17 14:28:23 +0200

    Lint fix

--- preferred-languages.php
+++ preferred-languages.php
@@ -36,5 +36,5 @@
  */
 require_once __DIR__ . '/inc/functions.php';
 
-// We need to load before plugins_loaded, see https://core.trac.wordpress.org/ticket/58546
+// We need to load before plugins_loaded, see https://core.trac.wordpress.org/ticket/58546.
 preferred_languages_boot();
