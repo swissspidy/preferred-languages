4e58d00f9c2c23e7fb666b3964ec81f5dcd62c60
Pascal Birchler <pascalb@google.com>
2023-07-19 11:00:36 +0200

    Fix version

--- preferred-languages.php
+++ preferred-languages.php
@@ -3,7 +3,7 @@
  * Plugin Name: Preferred Languages
  * Plugin URI:  https://github.com/swissspidy/preferred-languages/
  * Description: Choose languages for displaying WordPress in, in order of preference.
- * Version:     2.0.0
+ * Version:     2.0.1
  * Author:      Pascal Birchler
  * Author URI:  https://pascalbirchler.com
  * License:     GPL-2.0+
