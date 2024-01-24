a98803acccfba324e4d3e365a1c6500274e47c5b
Pascal Birchler <pascalb@google.com>
2024-01-24 22:02:16 +0100

    update domain

--- tests/e2e/plugins/no-merge-translations/no-merge-translations.php
+++ tests/e2e/plugins/no-merge-translations/no-merge-translations.php
@@ -4,7 +4,7 @@ Plugin Name: No Merge Translations
 Plugin URI: https://wordpress.org/
 Description: For testing purposes only.
 Version: 1.0.0
-Text Domain: merge-translations
+Text Domain: no-merge-translations
 Domain Path: languages/
 */
 
