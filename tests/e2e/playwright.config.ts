066f04f825823bcf5879371d429043647a481c7b
Pascal Birchler <pascalb@google.com>
2024-05-10 10:57:49 +0200

    Re-enable other browsers

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -11,16 +11,16 @@ const config = defineConfig( {
 				...devices[ 'Desktop Chrome' ],
 			},
 		},
-		// {
-		// 	name: 'webkit',
-		// 	use: { ...devices[ 'Desktop Safari' ] },
-		// },
-		// {
-		// 	name: 'firefox',
-		// 	use: {
-		// 		...devices[ 'Desktop Firefox' ],
-		// 	},
-		// },
+		{
+			name: 'webkit',
+			use: { ...devices[ 'Desktop Safari' ] },
+		},
+		{
+			name: 'firefox',
+			use: {
+				...devices[ 'Desktop Firefox' ],
+			},
+		},
 	],
 } );
 
