773bcae9a4ff0a73b3f945ce782113b80aa52209
Pascal Birchler <pascalb@google.com>
2025-01-13 09:44:08 +0100

    Add `__next40pxDefaultSize` to `SelectControl`

--- src/components/InactiveLocales.tsx
+++ src/components/InactiveLocales.tsx
@@ -113,6 +113,7 @@ function InactiveLocalesSelect( {
 			onChange={ onChange }
 			disabled={ ! hasItems }
 			__nextHasNoMarginBottom
+			__next40pxDefaultSize
 		>
 			{ installedLanguages.length > 0 && (
 				<optgroup
